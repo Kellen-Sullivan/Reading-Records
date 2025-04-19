a493e736878ecc7759294a35484ca932f57e58e4
Kellen-Sullivan <kellen.r.sullivan@gmail.com>
2025-04-18 21:04:06 -0700

    connected to db

--- app.js
+++ app.js
@@ -7,32 +7,15 @@ PORT        = 9124;                 // Set a port number at the top so it's easy
 app.get('/', function(req, res)
     {
         // Define our queries
-        query1 = 'DROP TABLE IF EXISTS books;'
-        query2 = 'CREATE TABLE books(title VARCHAR(255));'
-        query3 = 'INSERT INTO books (title) VALUES ("1984");'
         query4 = 'SELECT * FROM books;'
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+        // SELECT *...
+        db.pool.query(query4, function(err, results, fields){
 
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });                                        
+            // Send the results to the browser
+            res.send(JSON.stringify(results));
+        })
+    })                                      
 
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
